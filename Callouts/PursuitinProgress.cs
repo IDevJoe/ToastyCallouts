3eb8390c974b6c829534a8a786ba73e83b7b9810
ToastinYou <toastinyou@gmx.com>
2017-09-23 15:46:29 -0400

    Refactoring and made use of new Util functions

--- Callouts/PursuitinProgress.cs
+++ Callouts/PursuitinProgress.cs
@@ -1,8 +1,6 @@
 ﻿using LSPD_First_Response.Mod.API;
 using LSPD_First_Response.Mod.Callouts;
 using Rage;
-using System;
-using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace ToastyCallouts.Callouts
@@ -21,47 +19,7 @@ namespace ToastyCallouts.Callouts
         {
             _sP = Spawnpoints.GetGoodSpawnpoint();
 
-            _suspectVehicle = new Vehicle("BJXL", _sP) { IsPersistent = true };
-            _suspectPed = new Ped(_sP) { IsPersistent = true, BlockPermanentEvents = true };
-
-            _officerVehicle = Extensions.PoliceCar(_suspectVehicle.GetOffsetPositionFront(-15f));
-            _officerPed = Extensions.PolicePed(_suspectVehicle.GetOffsetPositionFront(-15f));
-
-            if (_suspectVehicle && _suspectPed && _officerVehicle && _officerPed)
-            {
-                _suspectPed.WarpIntoVehicle(_suspectVehicle, -1);
-                _officerPed.WarpIntoVehicle(_officerVehicle, -1);
-
-                _suspectVehicle.Heading = Extensions.ClosestVehicleHeading(_sP);
-                _officerVehicle.Heading = _suspectVehicle.Heading;
-
-                Extensions.DebugLog("Vehicles spawned = " + _suspectVehicle.Model + " and " + _officerVehicle.Model);
-                Extensions.SpectateCameraNormal(_officerVehicle);
-            }
-            else
-            {
-                try
-                {
-                    if (!_suspectVehicle) throw new NullReferenceException("Suspect Vehicle");
-                    else if (!_suspectPed) throw new NullReferenceException("Suspect Ped");
-                    else if (!_officerVehicle) throw new NullReferenceException("Officer Vehicle");
-                    else if (!_officerPed) throw new NullReferenceException("Officer Ped");
-                    else throw new InvalidOperationException();
-                }
-                catch (ArgumentException ex)
-                {
-                    Extensions.PrintRecursiveExceptions(ex);
-                }
-                catch (InvalidOperationException ex)
-                {
-                    Extensions.PrintRecursiveExceptions(ex);
-                }
-
-                Game.DisplayNotification("Toasty Callouts has encountered an error, ending the callout to prevent any crashes.");
-                End();
-            }
-
-            ShowCalloutAreaBlipBeforeAccepting(_sP, 50f);
+            ShowCalloutAreaBlipBeforeAccepting(_sP, 100f);
             CalloutMessage = "Pursuit in Progress";
             CalloutPosition = _sP;
 
@@ -72,12 +30,31 @@ namespace ToastyCallouts.Callouts
 
         public override void OnCalloutDisplayed()
         {
-            base.OnCalloutDisplayed();
-        }
+            _suspectVehicle = new Vehicle("BJXL", _sP) { IsPersistent = true };
+            _suspectPed = new Ped(_sP) { IsPersistent = true, BlockPermanentEvents = true };
 
-        public override void OnCalloutNotAccepted()
-        {
-            base.OnCalloutNotAccepted();
+            _officerVehicle = Util.PoliceCar(_suspectVehicle.GetOffsetPositionFront(-15f));
+            _officerPed = Util.PolicePed(_suspectVehicle.GetOffsetPositionFront(-15f));
+
+            if (_suspectVehicle && _suspectPed && _officerVehicle && _officerPed)
+            {
+                _suspectPed.WarpIntoVehicle(_suspectVehicle, -1);
+                _officerPed.WarpIntoVehicle(_officerVehicle, -1);
+
+                _suspectVehicle.Heading = Natives.ClosestVehicleHeading(_sP);
+                _officerVehicle.Heading = _suspectVehicle.Heading;
+
+                _suspectVehicle.BeginRollingStart();
+                _officerVehicle.BeginRollingStart();
+                Util.Log("Vehicles spawned = " + _suspectVehicle.Model.Name + " and " + _officerVehicle.Model.Name, 0);
+            }
+            else
+            {
+                Cleaning.CatchInvalidObjects(new Entity[] { _suspectVehicle, _suspectPed, _officerVehicle, _officerPed });
+                End();
+            }
+
+            base.OnCalloutDisplayed();
         }
 
         public override bool OnCalloutAccepted()
@@ -86,33 +63,27 @@ namespace ToastyCallouts.Callouts
             Functions.AddPedToPursuit(_pursuit, _suspectPed);
             Functions.AddCopToPursuit(_pursuit, _officerPed);
             Functions.SetPursuitIsActiveForPlayer(_pursuit, true);
-            Functions.SetPursuitCopsCanJoin(_pursuit, false);
-            PursuitVisual.Start(_pursuit, true);
 
             _suspectBlip = new Blip(_suspectVehicle)
             {
                 Alpha = 0f,
                 IsRouteEnabled = true
             };
-
             _suspectBlip.SetStandardColor(CalloutStandardization.BlipTypes.ENEMY);
 
+            Util.SpectateCameraToggler(_officerPed, _suspectPed);
             return base.OnCalloutAccepted();
         }
 
         public override void Process()
         {
             if (Game.IsKeyDown(Keys.End)) End();
-
-            if (Game.IsKeyDown(Keys.F8)) _officerPed.Kill();
-            if (Game.IsKeyDown(Keys.F9)) _suspectVehicle.IsPositionFrozen = true;
-
             base.Process();
         }
 
         public override void End()
         {
-            Extensions.EndAndClean(new Entity[] { _suspectPed, _suspectVehicle, _officerPed, _officerVehicle }, new[] { _suspectBlip }, new[] { _pursuit });
+            Cleaning.EndAndClean(new Entity[] { _suspectPed, _suspectVehicle, _officerPed, _officerVehicle }, new[] { _suspectBlip }, new[] { _pursuit });
 
             base.End();
         }
